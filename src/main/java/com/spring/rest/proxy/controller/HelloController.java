4b653b3b4f0d7e46da8b7a1a844985da21f63bd8
Mohd-Shahzad-Malik <szdmalik7795@gmail.com>
2022-09-06 15:05:04 +0530

    Added User form

--- src/main/java/com/spring/rest/proxy/controller/HelloController.java
+++ src/main/java/com/spring/rest/proxy/controller/HelloController.java
@@ -1,11 +1,24 @@
 package com.spring.rest.proxy.controller;
 
+import com.spring.rest.proxy.utilites.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
 
 @Controller
 public class HelloController {
 
-    public String home(){
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("user",new User());
         return "Hello";
+    }
+
+    @RequestMapping(value="/processForm", method = RequestMethod.POST)
+    public String processForm(User user) {
+        return "Result";
     }
 }
---
+++ src/main/java/com/spring/rest/proxy/utilites/User.java
@@ -0,0 +1,24 @@
+package com.spring.rest.proxy.utilites;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class User {
+    private String fullName;
+    private int age;
+    private boolean employed;
+    private String gender;
+
+    public User()
+    {}
+
+    public User(String fullName, int age, boolean employed, String gender) {
+        this.fullName = fullName;
+        this.age = age;
+        this.employed = employed;
+        this.gender = gender;
+    }
+}
